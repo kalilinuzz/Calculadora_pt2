4f7f408ff8d5c9a145b9d96c1aa9e2df3e162cf2
sanco <sanco@DESKTOP-2R0CH3O>
2021-03-17 22:04:40 +0100

    Primera versión

---
+++ Calculadoraç/src/calculadora/Calculadora.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author sanco
+ */
+public class Calculadora {
+    
+    private int res;
+    
+    Calculadora(){
+    }
+    
+    public int suma(int num1,int num2){
+        this.res=num1+num2;
+        return this.res;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
