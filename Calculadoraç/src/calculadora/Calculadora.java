deb6dc61c294fc21f37961e7d944a27b9ea14c2f
Santiago <kalilinuzz@gmail.com>
2021-03-19 23:49:52 +0100

    Modificacion apartado 8

--- Calculadoraç/src/calculadora/Calculadora.java
+++ Calculadoraç/src/calculadora/Calculadora.java
@@ -30,7 +30,7 @@ public class Calculadora {
     */
     
     /*
-    Modificación desde rama2 Modificado
+    Modificación desde rama2 modificado 
     */
     public static void main(String[] args) {
         // TODO code application logic here
